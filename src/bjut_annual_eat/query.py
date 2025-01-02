b8143b2851c8509dc518cf1b8cfbe8a8e61908d1
Nagi-ovo <13264500190@163.com>
2025-01-02 20:26:09 +0800

    fix: wrong config name

--- src/bjut_annual_eat/query.py
+++ src/bjut_annual_eat/query.py
@@ -4,7 +4,7 @@ import yaml
 
 def load_config():
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    config_path = os.path.join(current_dir, 'config.yaml.example')
+    config_path = os.path.join(current_dir, 'config.yaml')
     
     with open(config_path, 'r', encoding='utf-8') as f:
         return yaml.safe_load(f)
